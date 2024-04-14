75dfd18b1fda60123ecd80c1cb929f01c814acc5
Vaneeza-Khan99 <160541856+Vaneeza-Khan99@users.noreply.github.com>
2024-04-15 01:18:28 +0500

    Created a To Do List

---
+++ main.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos = [];
+let condition : boolean = true;
+
+while(condition)
+{let todosQuestions = await inquirer.prompt(
+    [
+        {
+             name:"firstQuestion",
+             type:"input",
+             message:"What would you like to add in your todoList"
+        },
+        {
+            name:"secondQuestion",
+            type:"confirm",
+            message:"What would you like to add more to your todoList",
+            default:"true"
+        }
+    ]
+);
+todos.push(todosQuestions.firstQuestion);
+condition = todosQuestions.secondQuestion
+
+console.log(todos)
+
+}
